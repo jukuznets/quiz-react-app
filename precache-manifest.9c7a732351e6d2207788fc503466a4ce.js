fcf86e663bad6e86fe414356af6aae412a7a06e9
Julia <yuliazenobia@gmail.com>
2020-09-05 15:10:54 +0300

    Updates

---
+++ precache-manifest.9c7a732351e6d2207788fc503466a4ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2f21dbb9437da89b546fcf79bc88c60",
+    "url": "/quiz-react-app/index.html"
+  },
+  {
+    "revision": "44c3a40a77a0f1589ddc",
+    "url": "/quiz-react-app/static/css/main.4eee6575.chunk.css"
+  },
+  {
+    "revision": "7abe38b47505c42d57d2",
+    "url": "/quiz-react-app/static/js/2.c54a9235.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-react-app/static/js/2.c54a9235.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44c3a40a77a0f1589ddc",
+    "url": "/quiz-react-app/static/js/main.db98ecd4.chunk.js"
+  },
+  {
+    "revision": "06aff71537d8a7a9e0c4",
+    "url": "/quiz-react-app/static/js/runtime-main.c8e6bfc5.js"
+  }
+]);
